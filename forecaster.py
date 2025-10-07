e75514d18cdd47d9db17a16474ef5c75faf74d63
Cole <colekoryto17@gmail.com>
2025-10-07 15:29:42 -0500

    WIP LSTM

--- forecaster.py
+++ forecaster.py
@@ -1,62 +1,153 @@
+import datetime
+import math
+
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from pyarrow.compute import scalar
-from tensorflow.keras.layers import Dense, LSTM, Input
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from tensorflow.keras.layers import Dense, LSTM, Input, Dropout
 from tensorflow.keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
 
 class Forecaster:
 
     def __init__(self):
-        price_df = pd.read_parquet("./data/^SPX.parquet")
+        self.price_df = pd.read_parquet("./data/^SPX.parquet")
         start_test_date = "2025-01-01"
-        self.train_data = price_df.loc[:start_test_date]
-        self.test_data = price_df.loc[start_test_date:]
+        # self.train_data = price_df.loc[:start_test_date]
+        # self.test_data = price_df.loc[start_test_date:]
+
+        # TODO temp
+        # self.train_data = price_df.loc[:]
 
 
-    def run_LSTM(self):
+    def run_LSTM(self, n_lookback = 30, n_forecast = 53):
+
+        # Splits data into test and training set
+        train_data = self.price_df.iloc[:-n_forecast]
+        test_data = self.price_df.iloc[-n_forecast:]
+
         # Scales the data using Min Max
         scaler = MinMaxScaler(feature_range=(0, 1))
-        scaler = scaler.fit(self.train_data)
-        scaled_train = scaler.transform(self.train_data)
+        scaler = scaler.fit(train_data)
+        scaled_train = scaler.transform(train_data)
 
         # Generates inputs and outputs based on lookback and forcasting periods
-        n_lookback = 30
-        n_forecast = 30
+
         x_train = []
         y_train = []
         for i in range(n_lookback, len(scaled_train) - n_forecast + 1):
             x_train.append(scaled_train[i - n_lookback: i])
-            y_train.append(scaled_train[i: i + n_lookback])
+            y_train.append(scaled_train[i: i + n_forecast])
 
         # Convert lists to numpy arrays so that Input layer works
         x_train = np.array(x_train)
         y_train = np.array(y_train)
 
         # Creates LSTM model
+        # model = Sequential()
+        # model.add(Input(shape=(n_lookback, 1)))
+        # model.add(LSTM(units=50, return_sequences=True))
+        # model.add(LSTM(units=50))
+        # model.add(Dense(n_forecast))
+        # model.compile(loss='mean_squared_error', optimizer='adam')
+        # model.fit(x_train, y_train, epochs=100, batch_size=32, verbose=2)
+        """
+        MSE: 18081.942519307137
+        MAE: 120.39972330729167
+        RMSE: 134.46911362579564
+        MAPE: 0.020008558931045105
+        maybe with epoc 10
+        """
+
+
+        # Initialize model
         model = Sequential()
         model.add(Input(shape=(n_lookback, 1)))
+        # LSTM layer 1
         model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.20))
+        # LSTM layer 2
+        model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.20))
+        # LSTM layer 3
+        model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.20))
+        # LSTM layer 4
+        model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.20))
+        # LSTM layer 5
+        model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.20))
+        # LSTM layer 6
+        model.add(LSTM(units=50, return_sequences=True))
+        model.add(Dropout(0.20))
+        # LSTM layer 7
         model.add(LSTM(units=50))
+        model.add(Dropout(0.20))
+        # final layer
         model.add(Dense(n_forecast))
+        model.summary()
+
         model.compile(loss='mean_squared_error', optimizer='adam')
-        model.fit(x_train, y_train, epochs=100, batch_size=32, verbose=2)
+        model.fit(x_train, y_train, epochs=10, batch_size=32, verbose=2)
+        """
+        MSE: 7435.416274722417
+        MAE: 66.07750651041667
+        RMSE: 86.2288598714051
+        MAPE: 0.01106870861961337
+        """
+
 
         # Generates predictions on test set using last training data
-        x_pred = scaled_train[-n_lookback:]
-        y_pred = scaler.inverse_transform(model.predict(x_pred.reshape(1, n_lookback, 1)))
+        # x_pred = scaled_train[-n_lookback - n_forecast:-n_forecast] # Gets enough data to predict last window of training data
+        x_pred = scaled_train[-n_lookback:] # Gets enough data to predict last window of training data
+        y_pred = scaler.inverse_transform(model.predict(x_pred.reshape(1, n_lookback, 1))).flatten()
+        y_pred_df = pd.DataFrame(y_pred, columns=['Price'])
+
+        # plot all the series together
+        #TODO I do not think these lines are connecting properly
+        plt.figure(figsize=(10, 5), dpi=100)
+        plt.plot(train_data.index, train_data['Price'], label='Training data')
+        plt.plot(test_data, color='blue', label='Actual Stock Price')
+        plt.plot(test_data.index, y_pred, color='orange', label='Predicted Stock Price')
+
+        plt.title('SPX Price Prediction')
+        plt.xlabel('Time')
+        plt.ylabel('SPX Price')
+        plt.legend(loc='upper left', fontsize=8)
+        plt.show()
+
+        #report performance
+        mse = mean_squared_error(test_data['Price'], y_pred)
+        print('MSE: ' + str(mse))
+        mae = mean_absolute_error(test_data['Price'], y_pred)
+        print('MAE: ' + str(mae))
+        rmse = math.sqrt(mean_squared_error(test_data['Price'], y_pred))
+        print('RMSE: ' + str(rmse))
+        mape = np.mean(np.abs(y_pred - test_data['Price']) / np.abs(test_data['Price']))
+        print('MAPE: ' + str(mape))
+
+        # plt.figure(figsize=(12, 6))
+        # plt.plot(self.train_data.loc[self.train_data.index <= '2020-11-18']['Price'], 'b', label="Original Price")
+        # plt.plot(self.train_data.loc[self.train_data.index <= '2020-11-18'].index, y_pred, 'r', label="Predicted Price")
+        # plt.xlabel('Time')
+        # plt.ylabel('Price')
+        # plt.legend()
+        # plt.grid(True)
+        # plt.show()
 
         # Organize the results in a dataframe for printing
-        df_past = pd.DataFrame(self.train_data)
+        df_past = pd.DataFrame(train_data)
         df_past.rename(columns={'Price': 'Actual'}, inplace=True)
         df_past['Forecast'] = np.nan
         df_past['Forecast'].iloc[-1] = df_past['Actual'].iloc[-1] # Ensures data is connected
         print(df_past)
 
         df_future = pd.DataFrame(columns=['Actual', 'Forecast'])
-        df_future['Forecast'] = y_pred.flatten()
+        df_future['Forecast'] = y_pred
         df_future['Actual'] = np.nan
         df_future.index = pd.date_range(start=df_past.index[-1] + pd.Timedelta(days=1), periods=n_forecast)  # set index
         print(df_future)
